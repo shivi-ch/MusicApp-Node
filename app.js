9b38704a3b3df6b0ced0a6856309ca6ba6bb6929
Your Name <shivanshchangle1@gmail.com>
2018-08-23 18:07:50 +0530

    port change

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var todoController = require('./controllers/musiccontrollers');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
 todoController(app);
 
-app.listen(3000);
+app.listen(port);
 console.log('listening on port 3000');
